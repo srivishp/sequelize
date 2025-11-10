b6e166d60c83fa2f26e96d58c09bd9ce58238a8e
Sri Vishnu Pasumarthi <srivishp@gmail.com>
2025-11-10 18:17:58 +0530

    Create a new user with middleware

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -17,17 +19,43 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// #Creating a middleware to fetch a user and use it in requests
+//-> This is run only for incoming requests & not during npm start
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// Associations/Relations between models
+// SQL concepts, check Sequelize docs for more info
+// Adding one to may relationships between Shop, Order, Item & User
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 // Syncs models to the DB by creating the appropriate tables and relations
 sequelize
-  .sync()
+  .sync({ force: true }) //* use { force: true } to drop and recreate tables on every server start
   .then((result) => {
-    // console.log(result);
-    app.listen();
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Luke", email: "luke@skywalker.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    //console.log(user);
+    app.listen(3000);
   })
   .catch((err) => {
     console.log(err);
---
+++ models/user.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
+const { name } = require("ejs");
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+});
+
+module.exports = User;
